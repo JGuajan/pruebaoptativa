2c9676d6f65d5b761cbae693b7fcc2b75dbc60af
JGuajan <betos_1416@yahoo.com>
2018-05-10 08:56:56 -0500

    JGUAJAN
    BASE DE DATOS

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=mysqlpablo.mysql.database.azure.com;dbname=pdomvcalumno;charset=utf8', 'pablolandeta@mysqlpablo', 'Pablo666');
+        $pdo = new PDO('mysql:host=pruebaopta.mysql.database.azure.com;dbname=pruebaopta;charset=utf8', 'jorgeguajan@pruebaopta', '1389Blaster28');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
